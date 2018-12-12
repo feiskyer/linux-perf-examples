303430cfba32f285740f62de0d49cd44b197f8af
Pengfei Ni <feiskyer@gmail.com>
2018-12-12 21:01:50 +0800

    Add -s option to setup buffer size

--- io-cached/README.md
+++ io-cached/README.md
@@ -2,7 +2,7 @@
 
 **《[Linux 性能优化实战](https://time.geekbang.org/column/intro/140)》第 xx 篇案例。**
 
-案例场景：
+案例场景为进程以固定间隔时间读取磁盘导致以下两种现象：
 
 * 直接 I/O 导致文件读取缓慢的问题
 * 缓存命中次数与 I/O 大小不匹配的问题
@@ -11,6 +11,25 @@
 
 ```sh
 make build
+```
+
+默认的运行命令为 `docker run --privileged --name=app -itd feisky/app:io-direct`。
+
+你还可以通过以下两个个选项来修改磁盘读取的行为：
+
+* `-d` 设置要读取的磁盘，默认前缀为 `/dev/sd` 或者 `/dev/xvd` 的磁盘
+* `-s` 设置每次读取的数据量大小，单位为字节，默认为 33554432（也就是 32MB）
+
+这些选项的使用方法为：
+
+```sh
+docker run --privileged --name=app -itd feisky/app:io-direct /app -d /dev/sdb -s 33554432
+```
+
+案例运行后，你可以执行 `docker logs app` 查看它的日志，正常情况下，可以看到如下的输出：
+
+```sh
+Reading data from disk /dev/sdb with buffer size 33554432
 ```
 
 ## 运行案例应用
--- io-cached/app-cached.c
+++ io-cached/app-cached.c
@@ -1,16 +1,18 @@
 #define _GNU_SOURCE
-#define BUF_SIZE 32*1024*1024
+#define BUF_SIZE 32 * 1024 * 1024
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 #include <unistd.h>
-#include <fcntl.h>
+#include <errno.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <string.h>
-#include <sys/file.h>
 #include <sys/time.h>
 #include <dirent.h>
+#include <string.h>
+#include <sys/file.h>
+#include <fcntl.h>
 #include <ctype.h>
 
 char *select_disk()
@@ -39,23 +41,51 @@ char *select_disk()
 	return NULL;
 }
 
-void app(const char* disk)
+long int get_value(char *str)
+{
+	char *endptr = NULL;
+	long int value = strtol(str, &endptr, 10);
+	if ((errno == ERANGE && (value == LONG_MAX || value == LONG_MIN)) || (errno != 0 && value == 0))
+	{
+		perror("strtol");
+		return -1;
+	}
+
+	if (endptr == str)
+	{
+		perror("not number");
+		return -1;
+	}
+
+	if (value <= 0)
+	{
+		perror("not positive number");
+		return -1;
+	}
+
+	return value;
+}
+
+void app(const char *disk, long int buffer_size)
 {
 	int flags = O_RDONLY | O_LARGEFILE;
 	int fd = open(disk, flags, 0755);
-	if (fd < 0) {
+	if (fd < 0)
+	{
 		perror("failed to open disk");
 		_exit(1);
 	}
 
 	unsigned char *buf;
 	struct timeval start, end;
-	posix_memalign((void **) &buf, 512, BUF_SIZE);
+	posix_memalign((void **)&buf, 512, buffer_size);
 	int read_bytes = 0;
 	gettimeofday(&start, NULL);
-	while (read_bytes < BUF_SIZE) {
-		int ret = read(fd, buf, BUF_SIZE);
-		if (ret < 0) {
+	while (read_bytes < buffer_size)
+	{
+		int ret = read(fd, buf, buffer_size);
+		if (ret < 0)
+		{
 			perror("failed to read contents");
 			close(fd);
 			free(buf);
@@ -66,7 +96,7 @@ void app(const char* disk)
 
 	gettimeofday(&end, NULL);
 	double total_time = (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1000000.0;
-	printf("Time used: %f s\n", total_time);
+	printf("Time used: %f s to read %ld bytes\n", total_time, buffer_size);
 	close(fd);
 	free(buf);
 }
@@ -76,13 +106,17 @@ int main(int argc, char **argv)
 	int status = 0;
 	int c = 0;
 	char *disk = NULL;
+	char *size = NULL;
 
-	while ((c = getopt(argc, argv, "d:")) != -1)
+	while ((c = getopt(argc, argv, "d:s:")) != -1)
 	{
 		switch (c)
 		{
 		case 'd':
 			disk = optarg;
+			break;
+		case 's':
+			size = optarg;
 			break;
 		case '?':
 			printf("Illegal option: -%c\n", isprint(optopt) ? optopt : '#');
@@ -101,10 +135,21 @@ int main(int argc, char **argv)
 		_exit(1);
 	}
 
-	printf("Reading data from disk %s\n", disk);
+	long int buffer_size = BUF_SIZE;
+	if (size != NULL)
+	{
+		buffer_size = get_value(size);
+		if (buffer_size < 0)
+		{
+			exit(1);
+		}
+	}
 
-	for (;;) {
-		app(disk);
+	printf("Reading data from disk %s with buffer size %ld\n", disk, buffer_size);
+
+	for (;;)
+	{
+		app(disk, buffer_size);
 		sleep(1);
 	}
 
