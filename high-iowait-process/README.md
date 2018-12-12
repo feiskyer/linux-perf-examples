3a96612b7ff3fc823c15294b2651d9e7e57f7fa2
Pengfei Ni <feiskyer@gmail.com>
2018-12-12 21:02:00 +0800

    Add -s option to setup buffer size

--- high-iowait-process/README.md
+++ high-iowait-process/README.md
@@ -2,7 +2,7 @@
 
 **《[Linux 性能优化实战](https://time.geekbang.org/column/intro/140)》第 07 和 08 篇案例。**
 
-案例场景：
+案例场景为多个子进程读取磁盘导致以下两种现象：
 
 * 系统的 iowait 使用率和平均负载过高
 * 大量僵尸进程
@@ -17,6 +17,26 @@ make build
 
 ```sh
 make run
+```
+
+默认的运行命令为 `docker run --privileged --name=app -itd feisky/app:iowait`。
+
+你还可以通过以下三个选项来修改磁盘读取的行为：
+
+* `-d` 设置要读取的磁盘，默认前缀为 `/dev/sd` 或者 `/dev/xvd` 的磁盘
+* `-s` 设置每次读取的数据量大小，单位为字节，默认为 67108864（也就是 64MB）
+* `-c` 设置每个子进程读取的次数，默认为 20 次，也就是读取 20*64MB 数据后子进程退出
+
+这些选项的使用方法为：
+
+```sh
+docker run --privileged --name=app -itd feisky/app:iowait /app -d /dev/sdb -s 67108864 -c 20
+```
+
+案例运行后，你可以执行 `docker logs app` 查看它的日志，正常情况下，可以看到如下的输出：
+
+```sh
+Reading data from disk /dev/sdb with buffer size 67108864 and count 20
 ```
 
 ## 停止应用
--- high-iowait-process/app.c
+++ high-iowait-process/app.c
@@ -1,9 +1,12 @@
 #define _GNU_SOURCE
 #define BUF_SIZE 64 * 1024 * 1024
+#define BUF_COUNT 20
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 #include <unistd.h>
+#include <errno.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
@@ -13,20 +16,24 @@
 #include <fcntl.h>
 #include <ctype.h>
 
-char* select_disk() {
-	DIR* dirptr = opendir("/dev/");
-	if (dirptr==NULL) {
+char *select_disk()
+{
+	DIR *dirptr = opendir("/dev/");
+	if (dirptr == NULL)
+	{
 		perror("Failed to open dir");
 		return NULL;
 	}
 
 	struct dirent *entry;
-	char* result = (char*)calloc(512, sizeof(char));
-	const char* sd_prefix = "sd";
-	const char* xvd_prefix = "xvd";
-	while(entry=readdir(dirptr)) {
-		if(strncmp(sd_prefix, entry->d_name, 2)==0 || strncmp(xvd_prefix, entry->d_name, 3)==0)  {
-			snprintf(result, 512*sizeof(char), "/dev/%s", entry->d_name);
+	char *result = (char *)calloc(512, sizeof(char));
+	const char *sd_prefix = "sd";
+	const char *xvd_prefix = "xvd";
+	while (entry = readdir(dirptr))
+	{
+		if (strncmp(sd_prefix, entry->d_name, 2) == 0 || strncmp(xvd_prefix, entry->d_name, 3) == 0)
+		{
+			snprintf(result, 512 * sizeof(char), "/dev/%s", entry->d_name);
 			return result;
 		}
 	}
@@ -35,20 +42,46 @@ char* select_disk() {
 	return NULL;
 }
 
-void sub_process(const char* disk)
+long int get_value(char *str)
 {
-	int fd = open(disk, O_RDONLY|O_DIRECT|O_LARGEFILE, 0755);
-	if (fd<0) {
+	char *endptr = NULL;
+	long int value = strtol(str, &endptr, 10);
+	if ((errno == ERANGE && (value == LONG_MAX || value == LONG_MIN)) || (errno != 0 && value == 0))
+	{
+		perror("strtol");
+		return -1;
+	}
+
+	if (endptr == str)
+	{
+		perror("not number");
+		return -1;
+	}
+
+	if (value <= 0)
+	{
+		perror("not positive number");
+		return -1;
+	}
+
+	return value;
+}
+
+void sub_process(const char *disk, size_t buffer_size, size_t count)
+{
+	int fd = open(disk, O_RDONLY | O_DIRECT | O_LARGEFILE, 0755);
+	if (fd < 0)
+	{
 		perror("failed to open disk");
 		_exit(1);
 	}
 
-	unsigned char* buf;
-	posix_memalign((void **)&buf, 512, BUF_SIZE);
-	int read_bytes=0;
-	while (read_bytes < 20 * BUF_SIZE)
+	unsigned char *buf;
+	posix_memalign((void **)&buf, 512, buffer_size);
+	size_t read_bytes = 0;
+	while (read_bytes < count * buffer_size)
 	{
-		int ret = read(fd, buf, BUF_SIZE);
+		size_t ret = read(fd, buf, buffer_size);
 		if (ret < 0)
 		{
 			perror("failed to read contents");
@@ -62,7 +95,6 @@ void sub_process(const char* disk)
 	close(fd);
 	free(buf);
 	_exit(0);
-
 }
 
 int main(int argc, char **argv)
@@ -70,13 +102,21 @@ int main(int argc, char **argv)
 	int status = 0;
 	int c = 0;
 	char *disk = NULL;
+	char *size = NULL;
+	char *count = NULL;
 
-	while ((c = getopt(argc, argv, "d:")) != -1)
+	while ((c = getopt(argc, argv, "d:s:c:")) != -1)
 	{
 		switch (c)
 		{
 		case 'd':
 			disk = optarg;
+			break;
+		case 's':
+			size = optarg;
+			break;
+		case 'c':
+			count = optarg;
 			break;
 		case '?':
 			printf("Illegal option: -%c\n", isprint(optopt) ? optopt : '#');
@@ -95,17 +135,39 @@ int main(int argc, char **argv)
 		_exit(1);
 	}
 
-	printf("Reading data from disk %s\n", disk);
+	long int buffer_size = BUF_SIZE;
+	long int buffer_count = BUF_COUNT;
+	if (size != NULL)
+	{
+		buffer_size = get_value(size);
+		if (buffer_size < 0)
+		{
+			exit(1);
+		}
+	}
+	if (count != NULL)
+	{
+		buffer_count = get_value(count);
+		if (buffer_count < 0)
+		{
+			exit(1);
+		}
+	}
 
-	for (;;) {
-		for (int i = 0; i < 2; i++) {
-			if(fork()== 0) {
-				sub_process(disk);
+	printf("Reading data from disk %s with buffer size %ld and count %ld\n", disk, buffer_size, buffer_count);
+
+	for (;;)
+	{
+		for (int i = 0; i < 2; i++)
+		{
+			if (fork() == 0)
+			{
+				sub_process(disk, buffer_size, buffer_count);
 			}
 		}
 		sleep(5);
 	}
 
-	while(wait(&status)>0);
+	while (wait(&status) > 0);
 	return 0;
 }
